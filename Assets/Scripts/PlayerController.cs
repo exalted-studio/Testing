9aab76b37a3945466f97f916db0875390b80bfe5
Morten Lindhardt <r3fuze@gmail.com>
2019-07-22 18:36:01 +0200

    Remove new Input System (not ready yet)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,38 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
-    InputMaster controls;
-
-    void Awake()
-    {
-        controls = new InputMaster();
-        controls.Player.Attack.performed += Attack;
-        controls.Player.Movement.performed += Move;
-    }
-
-    void Move(InputAction.CallbackContext ctx)
-    {
-        Vector2 direction = ctx.ReadValue<Vector2>();
-        Debug.Log("Moving: " + direction);
-    }
-
-    void Attack(InputAction.CallbackContext ctx)
-    {
-        Debug.Log("Bang bang!");
-    }
-
-    void OnEnable()
-    {
-        controls.Player.Enable();
-    }
-
-    void OnDisable()
-    {
-        controls.Player.Disable();
-    }
 }
