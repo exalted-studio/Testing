65544bd4b4d3979b7aef11d7f6dab931cec1bca9
Morten Lindhardt <r3fuze@gmail.com>
2019-07-25 01:44:19 +0200

    Add raytracing test (combine PlayerController and PlayerMovement later)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public LineRenderer lineRenderer;
+    Rigidbody2D rb;
 
+    // TODO: Make global somewhere?
+    private int PIXEL_RATIO = 1080 / 270;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition / PIXEL_RATIO);
+        Vector2 dir = (mousePosition - transform.position).normalized;
+
+        // Ignore collision with player (remember to apply "Player" layer to Player)
+        int layerMask = ~(LayerMask.GetMask("Player"));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100f, layerMask);
+
+        if (hit.collider != null)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+    }
 }
